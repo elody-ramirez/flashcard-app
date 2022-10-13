b7b964bffa4c53f45c64e022308861c60c3a3f95
elody-ramirez <elody.ramirez@gmail.com>
2022-10-12 22:05:48 -0400

    Created a quick flashcard app that uses the open trivia database, will be later using software engineering questions that I have saved in an api i make

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
-
+import { useEffect, useState, useRef } from 'react'
+import FlashCardList from './FlashCardList'
+import './app.css'
+import axios from 'axios'
 
 function App() {
+  const [flashcards, setFlashcards] = useState([])
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef()
+  const amountEl = useRef()
+
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api_category.php')
+      .then( res => {
+        setCategories(res.data.trivia_categories)
+      })
+  }, [])
+
+  const decodeString = (str) => {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+        .get('https://opentdb.com/api.php', {
+          params: {
+            amount: amountEl.current.value,
+            category: categoryEl.current.value
+          }
+        })
+        .then(res => {
+          setFlashcards(res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer)
+            const options = [
+              ...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - .5)
+            }
+        }))
+    })
+  }
+
   return (
-    <h1>Hello World</h1>
-  )
+		<>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option> 
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl}/>
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
+			<div className='container'>
+				<FlashCardList flashcards={flashcards} />
+			</div>
+		</>
+	)
 }
 
 export default App
